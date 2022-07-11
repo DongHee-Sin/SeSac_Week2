6ea5f9fc66d2a5f430fd0ddbbe4357e85dbd09e8
DongHee-Sin <voslr0601@naver.com>
2022-07-11 18:00:10 +0900

    버튼을 클릭하면 Label의 횟수가 업데이트되는 기능 추가

--- SeSacWeek2/EmotionDiaryViewController.swift
+++ SeSacWeek2/EmotionDiaryViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 
 class EmotionDiaryViewController: UIViewController {
 
+    // MARK: - Propertys
+    @IBOutlet var emotionalLabels: [UILabel]!
     
     
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emotionalLabels.forEach({
+            $0.adjustsFontSizeToFitWidth = true
+            $0.minimumScaleFactor = 0.5
+        })
     }
 
 
+    // MARK: - Methods
+    @IBAction func emotionButtonTapped(_ sender: UIButton) {
+        guard let index = Int(sender.currentTitle ?? "") else { return }
+        
+        guard let temp = emotionalLabels[index].text?.split(separator: " ").map({String($0)}) else { return }
+        
+        guard let count = Int(temp[1]) else { return }
+        
+        print("\(temp[0]) 눌림")
+        
+        emotionalLabels[index].text = "\(temp[0]) \(String(count + 1))"
+    }
 }
 
