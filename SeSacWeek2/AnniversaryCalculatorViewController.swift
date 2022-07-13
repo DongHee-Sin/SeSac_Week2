9fef9e3ecb2f3eb769db0203a75028d29b62b38a
DongHee-Sin <voslr0601@naver.com>
2022-07-13 15:49:25 +0900

    이미지에 Radius, 그림자 설정 추가

--- SeSacWeek2/AnniversaryCalculatorViewController.swift
+++ SeSacWeek2/AnniversaryCalculatorViewController.swift
@@ -9,8 +9,49 @@ import UIKit
 
 class AnniversaryCalculatorViewController: UIViewController {
 
+    // MARK: - Propertys
+    
+    
+    
+    
+    // MARK: - Outlet
+    @IBOutlet var forShadowViewList: [UIView]!
+    
+    @IBOutlet var imageList: [UIImageView]!
+    
+    
+    
+    
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
     }
     
+    
+    
+    // MARK: - Methods
+    func setupUI() {
+        imageList.forEach({ settingImage($0) })
+        
+        forShadowViewList.forEach({ addShadow($0, color: UIColor.black.cgColor, width: 0, height: 0, alpha: 0.3, radius: 10) })
+    }
+    
+    
+    // 이미지 설정
+    func settingImage(_ image: UIImageView) {
+        image.transform = image.transform.rotated(by: .pi / 2)
+        image.layer.cornerRadius = image.frame.height / 8
+    }
+    
+    
+    // 그림자 추가
+    func addShadow(_ to: UIView, color: CGColor, width: CGFloat, height: CGFloat, alpha: Float, radius: CGFloat) {
+        to.layer.shadowColor = color
+        to.layer.shadowOpacity = alpha
+        to.layer.shadowRadius = radius
+        to.layer.shadowOffset = CGSize(width: width, height: height)
+        to.layer.shadowPath = nil
+    }
 }
