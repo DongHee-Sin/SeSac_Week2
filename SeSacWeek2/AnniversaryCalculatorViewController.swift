64cd875d251b9b589754fcfa1cd51f60fd80f7c4
DongHee-Sin <voslr0601@naver.com>
2022-07-13 15:12:02 +0900

    기념일 계산기 UI 구성 & ViewController 연결

---
+++ SeSacWeek2/AnniversaryCalculatorViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AnniversaryCalculatorViewController.swift
+//  SeSacWeek2
+//
+//  Created by 신동희 on 2022/07/13.
+//
+
+import UIKit
+
+class AnniversaryCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
