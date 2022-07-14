8f6dfd55c96511099340e03661b4fd45886f1e25
DongHee-Sin <voslr0601@naver.com>
2022-07-14 16:19:41 +0900

    DatePicker에서 선택된 날짜 기준으로 기념일 Label의 Text를 변경하도록 기능 추가

--- SeSacWeek2/AnniversaryCalculatorViewController.swift
+++ SeSacWeek2/AnniversaryCalculatorViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class AnniversaryCalculatorViewController: UIViewController {
 
+    // MARK: - Propertys
+    let dateFormatter = DateFormatter()
+    
+    
+    
     // MARK: - Outlet
     @IBOutlet var forShadowViewList: [UIView]!
     
     @IBOutlet var imageList: [UIImageView]!
     
     @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet var anniversaryLabelList: [UILabel]!
     
     
     
@@ -24,29 +31,30 @@ class AnniversaryCalculatorViewController: UIViewController {
         super.viewDidLoad()
         
         setupUI()
-        
-        // DateFormatter : 알아보기 쉬운 날짜 + 시간대 (yyyy MM dd hh:mm:ss)
-        let format = DateFormatter()
-        format.dateFormat = "yyyy년 MM월 dd일"
-        
-        let result = format.string(from: Date())
-        
-        print(result, Date())    // 2022년 07월 14일 2022-07-14 03:26:20 +0000 (뒤에꺼는 영국표준시 기준으로 표시된거)
-        
-        let word = "2018년 09월 10일"
-        let dateResult = format.date(from: word)
-        print(dateResult)        // Optional(2018-09-09 15:00:00 +0000)
     }
     
     
     
     // MARK: - Methods
     func setupUI() {
+        anniversaryLabelList.forEach({ settingLabel($0) })
+        
         imageList.forEach({ settingImage($0) })
         
         forShadowViewList.forEach({ addShadow($0, color: UIColor.black.cgColor, width: 0, height: 0, alpha: 0.3, radius: 10) })
         
         settingDatePicker(datePicker)
+        
+        dateFormatter.dateFormat = "yyyy년\nMM월 dd일"
+        
+        datePickerValueChanged(datePicker)
+    }
+    
+    
+    // Label 설정
+    func settingLabel(_ label: UILabel) {
+        label.textAlignment = .center
+        label.numberOfLines = 2
     }
     
     
@@ -72,5 +80,43 @@ class AnniversaryCalculatorViewController: UIViewController {
         if #available(iOS 14.0, *) {
             picker.preferredDatePickerStyle = .inline
         }
+    }
+    
+    
+    func printToday() {
+        // DateFormatter : 알아보기 쉬운 날짜 + 시간대 (yyyy MM dd hh:mm:ss)
+        let format = DateFormatter()
+        format.dateFormat = "yyyy년 MM월 dd일"          // format 형식 지정
+        
+        let result = format.string(from: Date())      // Date To String 변환
+        
+        print(result, Date())    // 2022년 07월 14일
+        print(Date())            // 2022-07-14 03:26:20 +0000 (뒤에꺼는 영국표준시 기준으로 표시된거)
+        
+        
+        let word = "2018년 09월 10일"
+        let dateResult = format.date(from: word)      // String To Date (format형식에 맞지 않는 문자열을 입력하면 실패하기 때문에 OptionalType으로 반환)
+        
+        print(dateResult ?? "")        // Optional(2018-09-09 15:00:00 +0000)
+    }
+    
+    
+    func dateToString(_ date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+    
+    
+    func add100Day(_ date: Date) -> Date {
+        return date.addingTimeInterval(60 * 60 * 24 * 100)
+    }
+    
+    
+    @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
+        var date: Date = sender.date
+        
+        anniversaryLabelList.forEach({
+            date = add100Day(date)
+            $0.text = dateToString(date)
+        })
     }
 }
